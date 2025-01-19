9d21f1662ebfda34de8a89d90b57c978ee1659ac
karlrobeck <karlalferezfx@gmail.com>
2025-01-19 18:36:09 +0800

    add function parameters

--- hexgate-router/src/lib.rs
+++ hexgate-router/src/lib.rs
@@ -1,13 +1,36 @@
 mod routes {
+    use std::collections::BTreeMap;
+
     use axum::{
         Router,
+        extract::{Path, Query, State},
         routing::{delete, get, patch, post, put},
     };
+    use serde::{Deserialize, Serialize};
     use sqlx::{Pool, Postgres};
 
     #[derive(Clone)]
     pub struct HexgateRouter {
         db: Pool<Postgres>,
+    }
+
+    #[derive(Deserialize, Serialize)]
+    pub struct SQLSchemaTable {
+        schema: String,
+        table: String,
+    }
+
+    #[derive(Deserialize, Serialize)]
+    pub struct SQLFunction {
+        schema: String,
+        name: String,
+    }
+
+    #[derive(Deserialize, Serialize)]
+    pub struct SQLOperation {
+        limit: i32,
+        filter: String, // TODO: implement this properly for parsing
+        sort: String,   // TODO: implement this properly for parsing
     }
 
     impl HexgateRouter {
@@ -34,23 +57,43 @@ mod routes {
         }
 
         // select,update,insert,delete, execute function
-        pub async fn select_route() {
+        pub async fn select_route(
+            State(state): State<HexgateRouter>,
+            Path(path): Path<SQLSchemaTable>,
+            Query(query): Query<SQLOperation>,
+        ) {
             todo!("implement select operation for this route")
         }
 
-        pub async fn update_route() {
+        pub async fn update_route(
+            State(state): State<HexgateRouter>,
+            Path(path): Path<SQLSchemaTable>,
+            Query(query): Query<SQLOperation>,
+        ) {
             todo!("implement update operation for this route. use transaction here")
         }
 
-        pub async fn insert_route() {
+        pub async fn insert_route(
+            State(state): State<HexgateRouter>,
+            Path(path): Path<SQLSchemaTable>,
+            Query(query): Query<SQLOperation>,
+        ) {
             todo!("implement insert operation for this route. use transaction here")
         }
 
-        pub async fn delete_route() {
+        pub async fn delete_route(
+            State(state): State<HexgateRouter>,
+            Path(path): Path<SQLSchemaTable>,
+            Query(query): Query<SQLOperation>,
+        ) {
             todo!("implement delete operation for this route. use transaction here")
         }
 
-        pub async fn execute_function() {
+        pub async fn execute_function(
+            State(state): State<HexgateRouter>,
+            Path(path): Path<SQLFunction>,
+            Query(query): Query<serde_json::Value>, // key value pair json
+        ) {
             todo!("implement database function execution for this route. use transaction here")
         }
     }
